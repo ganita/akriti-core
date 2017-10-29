ba4d68692052248e5b741c1ecc8f87b9ed498d27
Sreejith Krishnan R <sreejith@ganita.io>
2017-10-29 20:41:13 +0530

    Vertical gravity impl

--- src/draw/linear_layout.rs
+++ src/draw/linear_layout.rs
@@ -129,7 +129,7 @@ impl Drawable for LinearLayout {
         let layout_gravity = &self.gravity;
 
         let mut axis_params: Option<AxisParams> = None;
-        let mut bottom_pos = 0f32;
+        let mut cross_axis_length = 0f32;
 
         // In first iteration we will compute the dimension of children to just wrap the content
         // and compute the total size of container
@@ -142,24 +142,32 @@ impl Drawable for LinearLayout {
                                                      layout_align,
                                                      child.params.align_self.as_ref());
 
-            bottom_pos = match *align {
+            cross_axis_length = match *align {
                 Align::Start | Align::End | Align::Center => {
-                    let local_bottom_pos = bottom_pos.max(child.drawable.bounding_box().height());
+                    let n_cross_axis_length = match self.gravity {
+                        Gravity::Vertical => cross_axis_length.max(child.drawable.bounding_box().width()),
+                        Gravity::Horizontal => cross_axis_length.max(child.drawable.bounding_box().height()),
+                    };
+
                     if let Some(ref mut params) = axis_params {
                         params.baseline_pos = match params.align {
                             Align::Start => params.baseline_pos,
 
                             Align::Center | Align::Baseline | Align::Axis =>
-                                ((local_bottom_pos - bottom_pos) / 2.) + params.baseline_pos,
+                                ((n_cross_axis_length - cross_axis_length) / 2.) + params.baseline_pos,
 
-                            Align::End => local_bottom_pos - (bottom_pos - params.baseline_pos),
+                            Align::End => n_cross_axis_length - (cross_axis_length - params.baseline_pos),
                         };
                     }
 
-                    local_bottom_pos
+                    n_cross_axis_length
                 }
                 Align::Baseline | Align::Axis => {
-                    let (n_height, n_axis_pos) = if let Some(ref mut params) = axis_params {
+                    if self.gravity == Gravity::Vertical {
+                        panic!("Cannot align to baseline or axis when using vertical gravity");
+                    }
+
+                    let (n_cross_axis_length, n_axis_pos) = if let Some(ref mut params) = axis_params {
                         let new_ascent = if *align == Align::Baseline {
                             params.baseline_pos().max(child.drawable.bounding_box().baseline_pos())
                         } else {
@@ -167,9 +175,9 @@ impl Drawable for LinearLayout {
                         };
 
                         let new_descent = if *align == Align::Baseline {
-                            child.drawable.bounding_box().baseline().max(bottom_pos - params.baseline_pos())
+                            child.drawable.bounding_box().baseline().max(cross_axis_length - params.baseline_pos())
                         } else {
-                            child.drawable.bounding_box().axis().max(bottom_pos - params.axis_pos())
+                            child.drawable.bounding_box().axis().max(cross_axis_length - params.axis_pos())
                         };
 
                         let basis_axis_pos = if *align == Align::Baseline {
@@ -179,15 +187,15 @@ impl Drawable for LinearLayout {
                         };
 
                         match params.align {
-                            Align::Start => (bottom_pos.max(basis_axis_pos + new_descent), basis_axis_pos),
-                            Align::Center => (bottom_pos, basis_axis_pos),
-                            Align::End => (new_ascent + bottom_pos - basis_axis_pos, new_ascent),
+                            Align::Start => (cross_axis_length.max(basis_axis_pos + new_descent), basis_axis_pos),
+                            Align::Center => (cross_axis_length, basis_axis_pos),
+                            Align::End => (new_ascent + cross_axis_length - basis_axis_pos, new_ascent),
                             Align::Baseline => (new_ascent + new_descent, new_ascent),
-                            Align::Axis => (bottom_pos.max(new_ascent + new_descent), new_ascent)
+                            Align::Axis => (cross_axis_length.max(new_ascent + new_descent), new_ascent)
                         }
                     } else {
                         (
-                            bottom_pos.max(child.drawable.bounding_box().height()),
+                            cross_axis_length.max(child.drawable.bounding_box().height()),
                             if *align == Align::Baseline {
                                 child.drawable.bounding_box().baseline_pos()
                             } else {
@@ -204,7 +212,7 @@ impl Drawable for LinearLayout {
                         }
                     }
 
-                    n_height
+                    n_cross_axis_length
                 }
             };
 
@@ -220,18 +228,21 @@ impl Drawable for LinearLayout {
             }
         }
 
-        let height = bottom_pos;
         let axis_params = axis_params.unwrap_or(AxisParams {
             align: Align::Start,
-            baseline_pos: height,
+            baseline_pos: cross_axis_length,
             axis_baseline_shift: 0.,
         });
 
-        let mut width = 0f32;
+        let mut main_axis_pen = 0f32;
         for child in self.children.iter_mut() {
             if child.params.cross_axis_bound_mode == CrossAxisBoundMode::FillParent {
-                child.drawable.calculate(context, -1., &MeasureMode::Wrap,
-                                         height, &MeasureMode::UpTo);
+                let (width, width_mode, height, height_mode) = match self.gravity {
+                    Gravity::Vertical => (cross_axis_length, MeasureMode::UpTo, -1., MeasureMode::Wrap),
+                    Gravity::Horizontal => (-1., MeasureMode::Wrap, cross_axis_length, MeasureMode::UpTo),
+                };
+
+                child.drawable.calculate(context, width, &width_mode, height, &height_mode);
             }
 
             let align = if child.params.cross_axis_bound_mode == CrossAxisBoundMode::FillParent {
@@ -244,23 +255,39 @@ impl Drawable for LinearLayout {
                 ).clone()
             };
 
-            let pos = match align {
-                Align::Start => Point::new(width, 0.),
-                Align::Center => Point::new(width, (height - child.drawable.bounding_box().height()) / 2.),
-                Align::End => Point::new(width, height - child.drawable.bounding_box().height()),
-                Align::Baseline => Point::new(width, axis_params.baseline_pos() - child.drawable.bounding_box().baseline_pos()),
-                Align::Axis => Point::new(width, axis_params.axis_pos() - child.drawable.bounding_box().axis_pos())
+            let child_cross_axis_length = match self.gravity {
+                Gravity::Vertical => child.drawable.bounding_box().width(),
+                Gravity::Horizontal => child.drawable.bounding_box().height(),
             };
 
-            child.point = pos;
+            let (main_axis, cross_axis) = match align {
+                Align::Start => (main_axis_pen, 0.),
+                Align::Center => (main_axis_pen, (cross_axis_length - child_cross_axis_length) / 2.),
+                Align::End => (main_axis_pen, cross_axis_length - child_cross_axis_length),
+                Align::Baseline => (main_axis_pen, axis_params.baseline_pos() - child.drawable.bounding_box().baseline_pos()),
+                Align::Axis => (main_axis_pen, axis_params.axis_pos() - child.drawable.bounding_box().axis_pos())
+            };
 
-            width += child.drawable.bounding_box().width();
+            child.point = match self.gravity {
+                Gravity::Vertical => Point::new(cross_axis, main_axis),
+                Gravity::Horizontal => Point::new(main_axis, cross_axis),
+            };
+
+            main_axis_pen += match self.gravity {
+                Gravity::Horizontal => child.drawable.bounding_box().width(),
+                Gravity::Vertical => child.drawable.bounding_box().height(),
+            }
         }
+
+        let (width, height) = match self.gravity {
+            Gravity::Horizontal => (main_axis_pen, cross_axis_length),
+            Gravity::Vertical => (cross_axis_length, main_axis_pen),
+        };
 
         self.bounding_box = BoundingBox {
             rect: Rect::new(width, height),
-            baseline: height - axis_params.baseline_pos(),
-            axis: height - axis_params.axis_pos(),
+            baseline: cross_axis_length - axis_params.baseline_pos(),
+            axis: cross_axis_length - axis_params.axis_pos(),
         };
     }
 
@@ -762,5 +789,145 @@ mod test {
         assert_eq!(ll.children[1].point, Point::new(10., 0.));
         assert_eq!(ll.children[1].drawable.bounding_box().height(), 50.);
         assert_eq!(ll.children[0].drawable.bounding_box().height(), 50.);
+    }
+
+    #[test]
+    fn it_aligns_vertical_layout_to_center() {
+        let context = test_context();
+        let mut ll = LinearLayout::new();
+        ll.gravity = Gravity::Vertical;
+        ll.layout_align = Align::Center;
+
+        let calculate = |ll: &mut LinearLayout| {
+            ll.calculate(&context, -1., &MeasureMode::Wrap, -1.,
+                         &MeasureMode::Wrap);
+        };
+
+        ll.add_child(Box::new(Fixed::new(30., 50., 10., 10.)),
+                     LinearLayoutParams::new());
+
+        calculate(&mut ll);
+
+        assert_eq!(ll.bounding_box().width(), 30.);
+        assert_eq!(ll.bounding_box().height(), 50.);
+        assert_eq!(ll.bounding_box().baseline(), 10.);
+        assert_eq!(ll.bounding_box().axis(), 10.);
+        assert_eq!(ll.children[0].point, Point::new(0., 0.));
+
+        ll.add_child(Box::new(Fixed::new(10., 20., 10., 10.)),
+                     LinearLayoutParams::new());
+        calculate(&mut ll);
+
+        assert_eq!(ll.bounding_box().width(), 30.);
+        assert_eq!(ll.bounding_box().height(), 70.);
+        assert_eq!(ll.children[0].point, Point::new(0., 0.));
+        assert_eq!(ll.children[1].point, Point::new(10., 50.));
+
+    }
+
+    #[test]
+    fn it_aligns_vertical_layout_to_left() {
+        let context = test_context();
+        let mut ll = LinearLayout::new();
+        ll.gravity = Gravity::Vertical;
+        ll.layout_align = Align::Start;
+
+        let calculate = |ll: &mut LinearLayout| {
+            ll.calculate(&context, -1., &MeasureMode::Wrap, -1.,
+                         &MeasureMode::Wrap);
+        };
+
+        ll.add_child(Box::new(Fixed::new(30., 50., 10., 10.)),
+                     LinearLayoutParams::new());
+
+        calculate(&mut ll);
+
+        assert_eq!(ll.bounding_box().width(), 30.);
+        assert_eq!(ll.bounding_box().height(), 50.);
+        assert_eq!(ll.bounding_box().baseline(), 10.);
+        assert_eq!(ll.bounding_box().axis(), 10.);
+        assert_eq!(ll.children[0].point, Point::new(0., 0.));
+
+        ll.add_child(Box::new(Fixed::new(10., 20., 10., 10.)),
+                     LinearLayoutParams::new());
+        calculate(&mut ll);
+
+        assert_eq!(ll.bounding_box().width(), 30.);
+        assert_eq!(ll.bounding_box().height(), 70.);
+        assert_eq!(ll.children[0].point, Point::new(0., 0.));
+        assert_eq!(ll.children[1].point, Point::new(0., 50.));
+
+    }
+
+    #[test]
+    fn it_aligns_vertical_layout_to_right() {
+        let context = test_context();
+        let mut ll = LinearLayout::new();
+        ll.gravity = Gravity::Vertical;
+        ll.layout_align = Align::End;
+
+        let calculate = |ll: &mut LinearLayout| {
+            ll.calculate(&context, -1., &MeasureMode::Wrap, -1.,
+                         &MeasureMode::Wrap);
+        };
+
+        ll.add_child(Box::new(Fixed::new(30., 50., 10., 10.)),
+                     LinearLayoutParams::new());
+
+        calculate(&mut ll);
+
+        assert_eq!(ll.bounding_box().width(), 30.);
+        assert_eq!(ll.bounding_box().height(), 50.);
+        assert_eq!(ll.bounding_box().baseline(), 10.);
+        assert_eq!(ll.bounding_box().axis(), 10.);
+        assert_eq!(ll.children[0].point, Point::new(0., 0.));
+
+        ll.add_child(Box::new(Fixed::new(10., 20., 10., 10.)),
+                     LinearLayoutParams::new());
+        calculate(&mut ll);
+
+        assert_eq!(ll.bounding_box().width(), 30.);
+        assert_eq!(ll.bounding_box().height(), 70.);
+        assert_eq!(ll.children[0].point, Point::new(0., 0.));
+        assert_eq!(ll.children[1].point, Point::new(20., 50.));
+
+    }
+
+    #[test]
+    fn vertical_layout_align_self_works() {
+        let context = test_context();
+        let mut ll = LinearLayout::new();
+        ll.gravity = Gravity::Vertical;
+        ll.layout_align = Align::Center;
+
+        let calculate = |ll: &mut LinearLayout| {
+            ll.calculate(&context, -1., &MeasureMode::Wrap, -1.,
+                         &MeasureMode::Wrap);
+        };
+
+        ll.add_child(Box::new(Fixed::new(100., 50., 10., 10.)),
+                     LinearLayoutParams::new());
+
+        calculate(&mut ll);
+
+        assert_eq!(ll.bounding_box().width(), 100.);
+        assert_eq!(ll.bounding_box().height(), 50.);
+        assert_eq!(ll.bounding_box().baseline(), 10.);
+        assert_eq!(ll.bounding_box().axis(), 10.);
+        assert_eq!(ll.children[0].point, Point::new(0., 0.));
+
+        ll.add_child(Box::new(Fixed::new(20., 20., 10., 10.)),
+                     LinearLayoutParams::new().with_align(Some(Align::Start)));
+
+        ll.add_child(Box::new(Fixed::new(20., 20., 10., 10.)),
+                     LinearLayoutParams::new().with_align(Some(Align::End)));
+        calculate(&mut ll);
+
+        assert_eq!(ll.bounding_box().width(), 100.);
+        assert_eq!(ll.bounding_box().height(), 90.);
+        assert_eq!(ll.children[0].point, Point::new(0., 0.));
+        assert_eq!(ll.children[1].point, Point::new(0., 50.));
+        assert_eq!(ll.children[2].point, Point::new(80., 70.));
+
     }
 }
